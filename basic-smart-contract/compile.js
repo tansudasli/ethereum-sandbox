c568979e2e6593734403526bc260ad5012b9e0b1
Tansu Dasli <tansudasli@gmail.com>
2019-10-25 11:54:16 +0300

    compiler script impl.

--- basic-smart-contract/compile.js
+++ basic-smart-contract/compile.js
@@ -1,2 +1,10 @@
 const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
 
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const sourceOfInbox = fs.readFileSync(inboxPath, 'utf-8');
+
+// console.log(solc.compile(sourceOfInbox, 1));
+
+solc.compile(sourceOfInbox, 1);
